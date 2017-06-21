57e85d58e750ef0e0e415c025ac5e1e534c0b3bb
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2017-06-20 17:11:53 -0700

    Republished samples with Fluent 1.1.1 reference

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using CoreFtp;
@@ -65,7 +65,7 @@ namespace ManageWebAppSourceControl
 
                 Utilities.Log("Deploying helloworld.War to " + app1Name + " through FTP...");
 
-                Utilities.UploadFileToFtp(
+                Utilities.UploadFileToWebApp(
                     app1.GetPublishingProfile(), 
                     Path.Combine(Utilities.ProjectPath, "Asset", "helloworld.war"));
 
